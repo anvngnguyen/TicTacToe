8dc18101c664108322fec8f82aad940710687d9d
An Nguyen <annguyen230693@gmail.com>
2019-09-07 13:39:06 -0700

    Implementing Drawing and Quitting scenario. Also remove some redundant code

--- src.py
+++ src.py
@@ -8,92 +8,80 @@ from typing import List, Tuple
 # 1. Implement menu screen
 # 2. Add documentation
 class TicTacToe:
-    # Screen Variables
-    WIDTH = 0
-    HEIGHT = 0
-    WINDOW = None
-    SCREEN = None
-    TRANSPARENT = None
-
-    # Player Pieces Variables
-    BLOCK_SIZE = 0
-    FONT = None
-
     def __init__(self):
+        # Player Pieces
+        pygame.font.init()
+        self.FONT = pygame.font.SysFont("Arial", 15)
+
         # Screen Setup
-        self.WIDTH = 630
-        self.HEIGHT = 630
+        self.MAX_PIECE = 30
+        self.BLOCK_SIZE = 20
+        self.WIDTH = self.MAX_PIECE * self.BLOCK_SIZE + self.MAX_PIECE
+        self.HEIGHT = self.MAX_PIECE * self.BLOCK_SIZE + self.MAX_PIECE
         self.WINDOW = pygame.display.set_mode([self.WIDTH, self.HEIGHT])
         self.SCREEN = pygame.display.get_surface()
         self.TRANSPARENT = pygame.Surface([self.WIDTH, self.HEIGHT])
         self.TRANSPARENT.set_alpha(255)
         self.TRANSPARENT.fill((255, 255, 255))
 
-        # Player Pieces
-        self.BLOCK_SIZE = 20
-        pygame.font.init()
-        self.FONT = pygame.font.SysFont("Arial", 15)
-
     def execute(self):
-        done, replay, grid, player, state = self.initialize_game()
+        done, replay, grid, player, piece_count, state = self.initialize_game()
         while not done:
-            done, replay, player, state = self.run_game(done, grid, player, state)
+            done, replay, player, piece_count, state = self.run_game(done, grid, player, piece_count, state)
         if replay:
             self.execute()
 
-    def initialize_game(self) -> Tuple[bool, bool, List[List], int, str]:
+    def initialize_game(self) -> Tuple[bool, bool, List[List], int, int, str]:
         pygame.init()
         player = 0
+        piece_count = 0
         done, replay = False, False
         state = "PLAYING"
         grid = self.initialize_grid()
-        return done, replay, grid, player, state
+        return done, replay, grid, player, piece_count, state
 
     def initialize_grid(self) -> List[List]:
         grid = []
-        for x in range(30):
+        for x in range(self.MAX_PIECE):
             grid.append([])
-            for y in range(30):
+            for y in range(self.MAX_PIECE):
                 rect = pygame.Rect(x * (self.BLOCK_SIZE + 1), y * (self.BLOCK_SIZE + 1),
                                    self.BLOCK_SIZE, self.BLOCK_SIZE)
                 grid[x].append([rect, None])
                 pygame.draw.rect(self.SCREEN, (255, 255, 255), rect)
         return grid
 
-    def run_game(self, done, grid, player, state):
+    def run_game(self, done, grid, player, piece_count, state):
         replay = False
         if state == "MENU":
             pass
         elif state == "PLAYING":
-            done, player, state = self.play(done, grid, player, state)
+            player, piece_count, state = self.play(grid, player, piece_count, state)
         elif state == "GAME-OVER":
-            done, replay = self.game_over(player)
+            done, replay = self.game_over(player, piece_count)
         pygame.display.flip()
-        return done, replay, player, state
+        return done, replay, player, piece_count, state
 
-    def play(self, done, grid, player, state):
+    def play(self, grid, player, piece_count, state):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return True, None, "GAME-OVER"
+                print("Quit")
+                return None, piece_count, "GAME-OVER"
             if event.type == pygame.MOUSEBUTTONUP:
-                player, state = self.make_move(grid, player, state)
-        return done, player, state
+                player, piece_count, state = self.make_move(grid, player, piece_count, state)
+        if piece_count == 25:
+            state = "GAME-OVER"
+        return player, piece_count, state
 
-    def make_move(self, grid, player, state):
-        change_player = False
+    def make_move(self, grid, player, piece_count, state):
         symbol = "X" if player == 0 else "O"
-
         i, j = math.floor(pygame.mouse.get_pos()[0] / 21), math.floor(pygame.mouse.get_pos()[1] / 21)
-
         if grid[i][j][1] is None:
-            change_player = True
             self.draw(grid[i][j], symbol)
             state = self.check_if_won(grid, symbol, i, j)
-
-        if change_player:
+            piece_count += 1
             player = 1 - player
-
-        return player, state
+        return player, piece_count, state
 
     def draw(self, cell, symbol):
         self.SCREEN.blit(self.FONT.render(symbol, True, (255, 0, 0)), (cell[0][0] + 5, cell[0][1] + 1))
@@ -119,7 +107,7 @@ class TicTacToe:
     def count_horizontally(self, counter, grid, i, j, left_limit, right_limit, symbol, win_type):
         start, end = None, None
         for index in range(i - left_limit, i + right_limit + 1):
-            if index in range(0, 30) and grid[index][j][1] == symbol:
+            if index in range(0, self.MAX_PIECE) and grid[index][j][1] == symbol:
                 counter, start, end = self.count_symbol(counter, index, j, start, end)
             else:
                 counter = 0 if counter < 5 else counter
@@ -128,7 +116,7 @@ class TicTacToe:
     def count_vertically(self, counter, grid, i, j, top_limit, bottom_limit, symbol, win_type):
         start, end = None, None
         for index in range(j - top_limit, j + bottom_limit + 1):
-            if index in range(0, 30) and grid[i][index][1] == symbol:
+            if index in range(0, self.MAX_PIECE) and grid[i][index][1] == symbol:
                 counter, start, end = self.count_symbol(counter, i, index, start, end)
             else:
                 counter = 0 if counter < 5 else counter
@@ -137,7 +125,8 @@ class TicTacToe:
     def count_diagonally(self, counter, grid, symbol, win_type, offset):
         start, end = None, None
         for i_offset, j_offset in offset:
-            if i_offset in range(0, 30) and j_offset in range(0, 30) and grid[i_offset][j_offset][1] == symbol:
+            if i_offset in range(0, self.MAX_PIECE) and j_offset in range(0, self.MAX_PIECE) and \
+                    grid[i_offset][j_offset][1] == symbol:
                 counter, start, end = self.count_symbol(counter, i_offset, j_offset, start, end)
             else:
                 counter = 0 if counter < 5 else counter
@@ -164,30 +153,28 @@ class TicTacToe:
                 return not self.check_diagonally(grid, start[0] - 1, start[1] + 1, end[0] + 1, end[1] - 1, symbol)
         return False
 
-    @staticmethod
-    def check_horizontally(grid, start_i, start_j, end_i, end_j, symbol):
+    def check_horizontally(self, grid, start_i, start_j, end_i, end_j, symbol):
         left = start_i >= 0 and grid[start_i][start_j][1] is not None and grid[start_i][start_j][1] != symbol
-        right = end_i < 30 and grid[end_i][end_j][1] is not None and grid[end_i][end_j][1] != symbol
+        right = end_i < self.MAX_PIECE and grid[end_i][end_j][1] is not None and grid[end_i][end_j][1] != symbol
         return left and right
 
-    @staticmethod
-    def check_vertically(grid, start_i, start_j, end_i, end_j, symbol):
+    def check_vertically(self, grid, start_i, start_j, end_i, end_j, symbol):
         top = start_j >= 0 and grid[start_i][start_j][1] is not None and grid[start_i][start_j][1] != symbol
-        bottom = end_j < 30 and grid[end_i][end_j][1] is not None and grid[end_i][end_j][1] != symbol
+        bottom = end_j < self.MAX_PIECE and grid[end_i][end_j][1] is not None and grid[end_i][end_j][1] != symbol
         return top and bottom
 
-    @staticmethod
-    def check_diagonally(grid, start_i, start_j, end_i, end_j, symbol):
-        first = (start_i in range(0, 30) and start_j in range(0, 30) and
+    def check_diagonally(self, grid, start_i, start_j, end_i, end_j, symbol):
+        first = (start_i in range(0, self.MAX_PIECE) and start_j in range(0, self.MAX_PIECE) and
                  grid[start_i][start_j][1] is not None and grid[start_i][start_j][1] != symbol)
-        second = (end_i < 30 and end_j < 30 and
+        second = (end_i < self.MAX_PIECE and end_j < self.MAX_PIECE and
                   grid[end_i][end_j][1] is not None and grid[end_i][end_j][1] != symbol)
         return first and second
 
     @staticmethod
-    def game_over(player):
+    def game_over(player, piece_count):
         screen = Screen()
-        replay = screen.show_game_over_screen(2 - player)
+        winner = 2 - player if player is not None else None
+        replay = screen.show_game_over_screen(winner, piece_count)
         return True, replay
 
 
@@ -199,12 +186,18 @@ class Screen:
         self.screen.wm_withdraw()
 
     @staticmethod
-    def show_game_over_screen(player):
-        msg_box = messagebox.askquestion("Game Over", f"Player {player} won. Play Again?")
+    def show_game_over_screen(winner, piece_count):
+        if piece_count == 25:
+            msg_box = messagebox.askquestion("Game Over", f"Draw. Play Again?")
+        elif winner is None:
+            return False
+        else:
+            msg_box = messagebox.askquestion("Game Over", f"Player {winner} won. Play Again?")
         if msg_box == "yes":
             return True
         else:
             return False
+
 
 game = TicTacToe()
 game.execute()
